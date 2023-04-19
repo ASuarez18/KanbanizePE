dcc1aa3557ef6d26900432cc58dbcc0b6f61010a
Alejandro Suarez <suga.gonza.18@gmail.com>
2023-04-19 00:06:48 -0600

    UseEffect Funcional

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,52 +12,99 @@ export const Home = (props) => {
   const [workspace, setWorkspace] = useState(null);
   const [boards, setBoards] = useState(null);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   const values = {
+  //     apikey: apikey
+  //   };
+  //   console.log(values);
+
+  //   const response = await fetch(`http://localhost:3013/workspaces`,
+  //   {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify(values)
+  //   });
+
+  //   const wspc = await response.json();
+
+  //   if (wspc.response !== 'Invalid apikey'){
+  //     setWorkspace(wspc);
+  //   }else{
+  //     // Mostrar error
+  //   }
+
+  //   const response2 = await fetch(`http://localhost:3013/boards`,
+  //   {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify(values)
+  //   });
+
+  //   const brds = await response2.json();
+
+  //   if (brds.response !== 'Invalid apikey'){
+  //     setBoards(brds);
+  //   }else{
+  //     // Mostrar error
+  //   }
+
+  //   console.log(workspace);
+  //   // console.log(boards);
+  // }
+
+  useEffect(() => {
     const values = {
       apikey: apikey
     };
     console.log(values);
 
-    const response = await fetch(`http://localhost:3013/workspaces`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(values)
-    });
+    const fetchWorkspace = async () => {
+      const response = await fetch(`http://localhost:3013/workspaces`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(values)
+      });
 
-    const wspc = await response.json();
+      const wspc = await response.json();
 
-    if (wspc.response !== 'Invalid apikey'){
-      setWorkspace(wspc);
-    }else{
-      // Mostrar error
+      if (wspc.response !== 'Invalid apikey'){
+        setWorkspace(wspc);
+      }else{
+        // Mostrar error
+      }
     }
 
-    const response2 = await fetch(`http://localhost:3013/boards`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(values)
-    });
+    const fetchBoards = async () => {
+      const response = await fetch(`http://localhost:3013/boards`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(values)
+      });
 
-    const brds = await response2.json();
+      const brds = await response.json();
 
-    if (brds.response !== 'Invalid apikey'){
-      setBoards(brds);
-    }else{
-      // Mostrar error
+      if (brds.response !== 'Invalid apikey'){
+        setBoards(brds);
+      }else{
+        // Mostrar error
+      }
     }
+    fetchBoards();
+    fetchWorkspace();
 
     console.log(workspace);
-    // console.log(boards);
-  }
-
-
+  }, []); 
   // //* Fetching Workspaces
   // useEffect(() => {
   //   const fetchWorkspace = async () => {
@@ -88,9 +135,9 @@ export const Home = (props) => {
     <div >
       {/* <img src={imagenes.img3}/> */}
       <div className="home">
-        <form onSubmit={handleSubmit}>
+        {/* <form onSubmit={handleSubmit}>
           <button type="submit">Cargar</button>
-        </form>
+        </form> */}
         <div className="workspaces-container">
           {/* {workspace && workspace.map((workspace) => (
             <WorkspaceDetails key={workspace.workspace_id} workspace={workspace} boards={boards}/>
