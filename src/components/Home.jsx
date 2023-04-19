2a867b62beed896d04fc31844db33742754c5f91
Alejandro Suarez <suga.gonza.18@gmail.com>
2023-04-18 18:42:46 -0600

    Mostrar workspaces y tablero casi funcional

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,43 +7,95 @@ import WorkspaceDetails from './WorkspaceDetails';
 
 
 export const Home = (props) => {
+  const apikey = localStorage.getItem('apikey');
+  // const navigate = useNavigate();
   const [workspace, setWorkspace] = useState(null);
   const [boards, setBoards] = useState(null);
 
-  //* Fetching Workspaces
-  useEffect(() => {
-    const fetchWorkspace = async () => {
-      const response = await fetch('http://localhost:3013/workspaces');
-      const json = await response.json();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const values = {
+      apikey: apikey
+    };
+    console.log(values);
 
-      if (response.ok) {
-        setWorkspace(json);
-      }
+    const response = await fetch(`http://localhost:3013/workspaces`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(values)
+    });
+
+    const wspc = await response.json();
+
+    if (wspc.response !== 'Invalid apikey'){
+      setWorkspace(wspc);
+    }else{
+      // Mostrar error
     }
-    fetchWorkspace();
-  }, []);
 
-  //* Fetching Boards
-  useEffect(() => {
-    const fetchBoards = async () => {
-      const response = await fetch('http://localhost:3013/boards');
-      const json = await response.json();
+    const response2 = await fetch(`http://localhost:3013/boards`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(values)
+    });
 
-      if (response.ok) {
-        setBoards(json);
-      }
+    const brds = await response2.json();
+
+    if (brds.response !== 'Invalid apikey'){
+      setBoards(brds);
+    }else{
+      // Mostrar error
     }
-    fetchBoards();
-  })
+
+    console.log(workspace);
+    // console.log(boards);
+  }
+
+
+  // //* Fetching Workspaces
+  // useEffect(() => {
+  //   const fetchWorkspace = async () => {
+  //     const response = await fetch('http://localhost:3013/workspaces');
+  //     const json = await response.json();
+
+  //     if (response.ok) {
+  //       setWorkspace(json);
+  //     }
+  //   }
+  //   fetchWorkspace();
+  // }, []);
+
+  // //* Fetching Boards
+  // useEffect(() => {
+  //   const fetchBoards = async () => {
+  //     const response = await fetch('http://localhost:3013/boards');
+  //     const json = await response.json();
+
+  //     if (response.ok) {
+  //       setBoards(json);
+  //     }
+  //   }
+  //   fetchBoards();
+  // })
 
   return (
     <div >
       {/* <img src={imagenes.img3}/> */}
       <div className="home">
+        <form onSubmit={handleSubmit}>
+          <button type="submit">Cargar</button>
+        </form>
         <div className="workspaces-container">
-          {workspace && workspace.map((workspace) => (
+          {/* {workspace && workspace.map((workspace) => (
             <WorkspaceDetails key={workspace.workspace_id} workspace={workspace} boards={boards}/>
-          ))} 
+          ))}  */}
+        
         </div>
       </div>
     </div>
