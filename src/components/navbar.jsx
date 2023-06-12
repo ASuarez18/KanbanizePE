f95104a5d5f41f2f08a33ccb3c2c501b15f5f51c
JoseAlbertoAR <106777143+JoseAlbertoAR@users.noreply.github.com>
2023-06-12 18:55:57 -0500

    Arreglar navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import i18n from 'i18next';
@@ -23,28 +24,32 @@ function MyNavbar() {
   }
 
   return (
-    <Navbar expand="lg" collapseOnSelect fixed="top">
-      <Navbar.Brand onClick={handleLogoClick}>
-        <img
-          src={logito}
-          alt="My logo"
-          width="45"
-          height="45"
-          className="d-inline-block align-top"
-          style={{ padding: "5px", cursor: "pointer" }}
-        />
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto">
-          <NavDropdown title={t('Language')} id="basic-nav-dropdown">
-            <NavDropdown.Item onClick={() => changeLanguage('es')}>{t('Spanish')}</NavDropdown.Item>
-            <NavDropdown.Item onClick={() => changeLanguage('en')}>{t('English')}</NavDropdown.Item>
-          </NavDropdown>
-          <Nav.Link onClick={handleLogout}>{t('Log out')}</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <div style={{ marginTop: "70px", display: 'flex' }}>
+      {/* Margen superior para desplazar los elementos */}
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect fixed="top">
+        <Navbar.Brand onClick={handleLogoClick}>
+          <img
+            src={logito}
+            alt="My logo"
+            width="45"
+            height="45"
+            className="d-inline-block align-top"
+            style={{ padding: "5px", cursor: "pointer" }}
+          />
+          <span style={{ color: "white", fontSize: "25px", marginLeft: "5px" }}>KANBANIZE PE</span>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto justify-content-center">
+            <NavDropdown title={t('Language')} id="basic-nav-dropdown">
+              <NavDropdown.Item onClick={() => changeLanguage('es')}>{t('Spanish')}</NavDropdown.Item>
+              <NavDropdown.Item onClick={() => changeLanguage('en')}>{t('English')}</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link onClick={handleLogout}>{t('Log out')}</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
   );
 }
 
